598f83b2fce016f3918516cf88524aa99a614a7c
Adarsh Anand <adarshanshu7@gmail.com>
2024-05-31 02:00:08 +0530

    updated

--- client/src/components/pages/login.js
+++ client/src/components/pages/login.js
@@ -1,14 +1,102 @@
-import React, {useContext} from 'react'
+import React, {useContext, useCallback} from 'react'
 import { Alert, Button, Form } from 'react-bootstrap'
 import '../styles/login.css'
 import { AuthContext } from '../../context/AuthContext'
+import Particles from "react-particles";
+import { loadSlim } from "tsparticles-slim"; // if you are going to use `loadSlim`
 
 function Login() {
 
   const { loginUser, loginError, loginInfo, updateLoginInfo, isloginLoading} = useContext(AuthContext);
 
+  const particlesInit = useCallback(async engine => {
+        console.log(engine);
+        // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        //await loadFull(engine);
+        await loadSlim(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async container => {
+        await console.log(container);
+    }, []);
+
   return (
     <>
+    <Particles
+            id="tsparticles"
+            init={particlesInit}
+            loaded={particlesLoaded}
+            options={{
+                background: {
+                    opacity: 0,
+                },
+                fpsLimit: 520,
+                interactivity: {
+                    events: {
+                        onClick: {
+                            enable: true,
+                            mode: "push",
+                        },
+                        onHover: {
+                            enable: true,
+                            mode: "repulse",
+                        },
+                        resize: true,
+                    },
+                    modes: {
+                        push: {
+                            quantity: 4,
+                        },
+                        repulse: {
+                            distance: 150,
+                            duration: 0.4,
+                        },
+                    },
+                },
+                particles: {
+                    color: {
+                        value: ["#FFFFFF"],
+
+                    },
+                    links: {
+                        color: ["#0000FF", "#00FF00", "#FF0000"],
+                        distance: 150,
+                        enable: true,
+                        opacity: 0.3,
+                        width: 1,
+                    },
+                    move: {
+                        direction: "none",
+                        enable: true,
+                        outModes: {
+                            default: "bounce",
+                        },
+                        random: false,
+                        speed: 1,
+                        straight: false,
+                    },
+                    number: {
+                        density: {
+                            enable: true,
+                            area: 2000,
+                        },
+                        value: 80,
+                    },
+                    opacity: {
+                        value: 0.5,
+                    },
+                    shape: {
+                        type: "circle",
+                    },
+                    size: {
+                        value: { min: 1, max: 3 },
+                    },
+                },
+                detectRetina: true,
+            }}
+        />
           <Form className='my-4' onSubmit={loginUser}>
 
           <h1>Login</h1>
--- client/src/components/pages/register.js
+++ client/src/components/pages/register.js
@@ -1,7 +1,9 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useCallback } from 'react';
 import { Alert, Button, Form, InputGroup } from 'react-bootstrap';
 import '../styles/register.css';
 import { AuthContext } from '../../context/AuthContext';
+import Particles from "react-particles";
+import { loadSlim } from "tsparticles-slim"; // if you are going to use `loadSlim`
 
 function Register() {
   const [showPassword, setShowPassword] = useState(false);
@@ -10,11 +12,97 @@ function Register() {
     setShowPassword(!showPassword);
   };
 
+  const particlesInit = useCallback(async engine => {
+        console.log(engine);
+        // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        //await loadFull(engine);
+        await loadSlim(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async container => {
+        await console.log(container);
+    }, []);
+
   const { registerInfo, updateRegisterInfo, registerUser, registerError, isRegisterLoading, registerSuccess } = useContext(AuthContext);
 
   return (
     <>
-      <Form className='my-4' onSubmit={registerUser}>
+    <Particles
+            id="tsparticles"
+            init={particlesInit}
+            loaded={particlesLoaded}
+            options={{
+                background: {
+                    opacity: 0,
+                },
+                fpsLimit: 520,
+                interactivity: {
+                    events: {
+                        onClick: {
+                            enable: true,
+                            mode: "push",
+                        },
+                        onHover: {
+                            enable: true,
+                            mode: "repulse",
+                        },
+                        resize: true,
+                    },
+                    modes: {
+                        push: {
+                            quantity: 4,
+                        },
+                        repulse: {
+                            distance: 150,
+                            duration: 0.4,
+                        },
+                    },
+                },
+                particles: {
+                    color: {
+                        value: ["#FFFFFF"],
+
+                    },
+                    links: {
+                        color: ["#0000FF", "#00FF00", "#FF0000"],
+                        distance: 150,
+                        enable: true,
+                        opacity: 0.3,
+                        width: 1,
+                    },
+                    move: {
+                        direction: "none",
+                        enable: true,
+                        outModes: {
+                            default: "bounce",
+                        },
+                        random: false,
+                        speed: 1,
+                        straight: false,
+                    },
+                    number: {
+                        density: {
+                            enable: true,
+                            area: 2000,
+                        },
+                        value: 80,
+                    },
+                    opacity: {
+                        value: 0.5,
+                    },
+                    shape: {
+                        type: "circle",
+                    },
+                    size: {
+                        value: { min: 1, max: 3 },
+                    },
+                },
+                detectRetina: true,
+            }}
+        />
+      <Form onSubmit={registerUser}>
         <h1>Register</h1>
 
         <Form.Group className="mb-3" controlId="formBasicUser">
